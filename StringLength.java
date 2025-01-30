acbd90e42631c0971dc5bae6ae50243ab923a422
godhalaakhila <akhilareddyg55@gmail.com>
2025-01-30 18:18:37 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,15 @@
+public class StringLength {
+    public static void main(String[] args) {
+        // Define a string directly
+        String input = "Hello, World!";
+
+        // Find the length of the string
+        int length = input.length();
+
+        // Display the length of the string
+        System.out.println("Length of the string: " + length);
+    }
+}
+ 
+    
+
